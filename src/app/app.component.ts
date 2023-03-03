049bfb718abf9f58cabdc2f96cbef88401afe7ef
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-03 15:53:14 -0500

    finally got a basic search up and running!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-search';
+
+  data = [
+    {id: 1, name: "Angular"},
+    {id: 2, name: "React"},
+    {id: 3, name: "Vue"},
+    {id: 4, name: "Bootstrap"},
+    {id: 5, name: "Foundation"},
+
+  ]
+
+  searchText = ''
 }
