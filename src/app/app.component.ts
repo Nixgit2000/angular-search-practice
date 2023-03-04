d2e5bf516b4f04aa49008fffef6dc56fdc1dae79
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-04 13:31:16 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   data = [
     {title: "Harry Potter", actor: "Daniel Radcliffe",  pic: "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg"},
     {title: "Percy Jackson", pic: "https://upload.wikimedia.org/wikipedia/en/d/db/Percy_Jackson_Portrait.jpg"},
-    {title: "Frodo Baggins",  pic: "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg"},
+    {title: "Frodo Baggins",  pic: "https://myhero.com/content/images/thumbs/0035521_frodo-baggins_400.jpeg"},
     {title: "Bilbo Baggins", pic: "https://i.pinimg.com/474x/7e/bb/3f/7ebb3f6e8b8b20e8f29c657dbc67870f.jpg"},
     {title: "Mario", pic: "https://media.istockphoto.com/id/458294725/photo/super-mario.jpg?s=612x612&w=0&k=20&c=4qlhFK7ACSlbuISSfbWjAe2VP1qqJDhZyCVJONPPx70="},
 
