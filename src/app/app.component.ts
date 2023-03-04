fc83aa3995c51fada9b70f95e233de7c01f516e0
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-04 13:03:07 -0500

    developing skeleton to display content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ export class AppComponent {
   title = 'angular-search';
 
   data = [
-    {id: 1, name: "Angular"},
-    {id: 2, name: "React"},
-    {id: 3, name: "Vue"},
-    {id: 4, name: "Bootstrap"},
-    {id: 5, name: "Foundation"},
+    {title: "Harry Potter", actor: "Daniel Radcliffe",  pic: "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg"},
+    {title: "Percy Jackson", pic: "https://upload.wikimedia.org/wikipedia/en/d/db/Percy_Jackson_Portrait.jpg"},
+    {title: "Frodo Baggins",  pic: "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg"},
+    {title: "Bilbo Baggins", pic: "https://i.pinimg.com/474x/7e/bb/3f/7ebb3f6e8b8b20e8f29c657dbc67870f.jpg"},
+    {title: "Mario", pic: "https://media.istockphoto.com/id/458294725/photo/super-mario.jpg?s=612x612&w=0&k=20&c=4qlhFK7ACSlbuISSfbWjAe2VP1qqJDhZyCVJONPPx70="},
 
   ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchPipe } from './search.pipe';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPipe
+    SearchPipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
