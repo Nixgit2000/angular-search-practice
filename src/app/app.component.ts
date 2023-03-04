679980d39155033cc1168ee0f9f9841cc434641d
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-04 15:21:19 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-search';
-
+  font = "HP"
+  fontSize = "25px"
   data = [
-    {title: "Harry Potter", actor: "Daniel Radcliffe",  pic: "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg"},
-    {title: "Percy Jackson", pic: "https://upload.wikimedia.org/wikipedia/en/d/db/Percy_Jackson_Portrait.jpg"},
-    {title: "Frodo Baggins",  pic: "https://myhero.com/content/images/thumbs/0035521_frodo-baggins_400.jpeg"},
-    {title: "Bilbo Baggins", pic: "https://i.pinimg.com/474x/7e/bb/3f/7ebb3f6e8b8b20e8f29c657dbc67870f.jpg"},
-    {title: "Mario", pic: "https://media.istockphoto.com/id/458294725/photo/super-mario.jpg?s=612x612&w=0&k=20&c=4qlhFK7ACSlbuISSfbWjAe2VP1qqJDhZyCVJONPPx70="},
-
+    {title: "Harry Potter", title2: "and the Sorcerer's Stone", font: "HP", fontSize: "25px", fontSize2: "20px", actor: "Daniel Radcliffe",  pic: "https://images.moviesanywhere.com/3d899a3e03eb1e3684a903168b601f47/1707fb29-a315-4f96-8dce-3b9fb5fbc219.jpg?w=2560&r=16x9"},
+    {title: "Harry Potter", title2: "and the Chamber of Secrets", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "70% 60%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/CoS/hp-cos-og.png"},
+    {title: "Harry Potter", title2: "and the Prisoner of Azkaban", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "70% 60%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/PoA/hp-poa-og-min.png"},
+    {title: "Harry Potter", title2: "and the Goblet of Fire", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "70% 60%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/GoF/hp-gof-og-min.png"},
+    {title: "Harry Potter", title2: "and the Order of the Phoenix", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe",  pic: "https://hbomax-images.warnermediacdn.com/images/GXssPaAiBJ1VGwwEAAABW/tile?size=640x360&partner=hbomaxcom&v=f8dbd8dc7f2ba00c5fa298123bf0dae2&host=art-gallery.api.hbo.com&language=en-us"},
+    {title: "Harry Potter", title2: "and the Half Blood Prince", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "40% 60%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/HBP/hp-hbp-og-min.png"},
+    {title: "Harry Potter", title2: "and the Deathly Hallows: Part I", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "90% 10%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/DH1/hp-dh1-desktop-min.jpg?downsize=1200:*&output-quality=70"},
+    {title: "Harry Potter", title2: "and the Deathly Hallows: Part I", font: "HP", fontSize: "25px",fontSize2: "20px", actor: "Daniel Radcliffe", backgroundPos: "10% 60%",  pic: "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/DH2/hp-dh2-og-min.png"},
+ 
   ]
 
   searchText = ''
